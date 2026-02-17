4a28318b1b610811d7a857da8ca1cc6537f0b527
Dr-Discount <trackerjack02@gmail.com>
2026-02-17 10:25:36 -0700

    Base Login/Home page
    Work in progress just pushing for a starting point.

---
+++ PRO150_Website/PRO150_Website/Components/Models/LoginInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PRO150_Website.Components.Models
+{
+    public class LoginInfo
+    {
+        [Required(ErrorMessage = "Email Rquried")]
+        public string Email { get; set; } = null;
+
+        [Required(ErrorMessage = "Password Required")]
+        public string Password { get; set; } = null;
+
+        public bool ValadateLogin()
+        {
+            //to be implmented
+            return true;
+        }
+    }
+}
