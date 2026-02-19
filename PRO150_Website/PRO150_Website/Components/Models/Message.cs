6a062711d567df130e5961a5af5a268507cc2147
Dr-Discount <trackerjack02@gmail.com>
2026-02-19 09:42:55 -0700

    chat page
    background messed up will fix later

---
+++ PRO150_Website/PRO150_Website/Components/Models/Message.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PRO150_Website.Components.Models
+{
+    public class Message
+    {
+        [Required]
+        public string message { get; set; }
+    }
+}
