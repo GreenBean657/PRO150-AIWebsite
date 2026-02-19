0a2dedeb78f417658607b6f6e82f25e76db5cd7c
Dr-Discount <trackerjack02@gmail.com>
2026-02-19 16:33:21 -0700

    fixed login style issue

--- PRO150_Website/PRO150_Website/obj/Debug/net10.0/PRO150_Website.AssemblyInfo.cs
+++ PRO150_Website/PRO150_Website/obj/Debug/net10.0/PRO150_Website.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRO150_Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a062711d567df130e5961a5af5a268507cc2147")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8891cf20e7dd5e816025219e93ed60b96515ef10")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRO150_Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRO150_Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
