817376ac2509b52bc4a6eb4b9a62d8a94810508a
GreenBean657 <greenbean@greencypher.tech>
2026-03-10 17:35:06 -0600

    Add chat model options

--- PRO150_Website/PRO150_Website/Components/Models/AIModel.cs
+++ PRO150_Website/PRO150_Website/Components/Models/AIModel.cs
@@ -1,11 +1,33 @@
-﻿namespace PRO150_Website.Components.Models
+﻿using System.Text.Json.Serialization;
+
+namespace PRO150_Website.Components.Models
 {
     public class AIModel
     {
         public string Model { get; set; }
     }
 
-    public static class AllAIModels {
-        public static List<string> AIModels = new() { "Model1", "Model2", "Model3" };
+    // Matches API response format
+    public class AIModelApi
+    {
+        public string id { get; set; }
+
+        [JsonPropertyName("object")]
+        public string Object { get; set; }
+
+        public string owned_by { get; set; }
+    }
+
+    public class AIModelListApi
+    {
+        public AIModelApi[] data { get; set; }
+
+        [JsonPropertyName("object")]
+        public string Object { get; set; }
+    }
+
+    public static class AllAIModels
+    {
+        public static List<AIModel> AIModels { get; set; } = new();
     }
 }
