beaaed2a16827bb179cf958e41109d05a0637cb1
Dr-Discount <trackerjack02@gmail.com>
2026-03-10 17:16:23 -0600

    dropdown for Ai models

---
+++ PRO150_Website/PRO150_Website/Components/Models/AIModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PRO150_Website.Components.Models
+{
+    public class AIModel
+    {
+        public string Model { get; set; }
+    }
+
+    public static class AllAIModels {
+        public static List<string> AIModels = new() { "Model1", "Model2", "Model3" };
+    }
+}
